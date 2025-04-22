232724388eb217e5a098e8dd596a23718bf59e55
Lea-M04 <leartamustafa1618@gmail.com>
2025-04-22 13:16:05 +0200

    rregullimi i nje errori ne import

--- prison_manager/src/App.js
+++ prison_manager/src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import RolePage from './frontend/roles/rolePage';
 import CellPage from './frontend/cells/cellPage';
 import './Bootstrap/css/sb-admin-2.css';
-import UserPage from './frontend/userPage';
-import LawyerPage from './frontend/lawyer/LawyerPage';
 import UserPage from './frontend/users/userPage';
+import LawyerPage from './frontend/lawyer/LawyerPage';
 import EmergencyContactPage from './frontend/emergencyContact/emergencyContactPage';
 import BudgetPage from './frontend/budget/budgetPage';
 
